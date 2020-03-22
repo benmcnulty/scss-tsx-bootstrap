a2ace783fce63a0088a95d1f7e668e2ada618bbe
Ben McNulty <benmcnulty@live.com>
2020-03-21 21:35:57 -0400

    Updates

---
+++ precache-manifest.80d8a9ac38f676056186425e89d4a006.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d06f71f8a448c1ffd0fbdb22744b0afb",
+    "url": "/scss-tsx-bootstrap/index.html"
+  },
+  {
+    "revision": "0955db38bfa7242e4eef",
+    "url": "/scss-tsx-bootstrap/static/css/main.a689ad72.chunk.css"
+  },
+  {
+    "revision": "b0dd059c275f4e4dab84",
+    "url": "/scss-tsx-bootstrap/static/js/2.5d889fe3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/scss-tsx-bootstrap/static/js/2.5d889fe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0955db38bfa7242e4eef",
+    "url": "/scss-tsx-bootstrap/static/js/main.19835fa0.chunk.js"
+  },
+  {
+    "revision": "987cc08eadbea1c07835",
+    "url": "/scss-tsx-bootstrap/static/js/runtime-main.b9db8281.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/scss-tsx-bootstrap/static/media/logo.5d5d9eef.svg"
+  }
+]);
